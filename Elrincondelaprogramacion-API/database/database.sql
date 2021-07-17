68b2b239e516ecb5309b7b46603e35435ea3b5eb
Pacorb94 <a@a.com>
2021-07-17 18:16:39 +0200

    modified database.sql

--- Elrincondelaprogramacion-API/database/database.sql
+++ Elrincondelaprogramacion-API/database/database.sql
@@ -3,13 +3,13 @@ use `El_rincon_de_la_programacion`;
 
 create table if not exists Users(
     id int(255) auto_increment not null,
-    nick varchar(50) not null,
+    nick varchar(50) unique not null,
     email varchar(50) unique not null,
     password varchar(50) not null,
     profile_image varchar(200),
     role varchar(50) not null,
-    created_at datetime,
-    updated_at datetime,
+    created_at datetime not null,
+    updated_at datetime not null,
     constraint pk_users primary key (id)
 )ENGINE=InnoDB;
 
@@ -21,8 +21,8 @@ create table if not exists Posts(
     content longtext not null,
     status varchar(50),
     image varchar(200),
-    created_at datetime,
-    updated_at datetime,
+    created_at datetime not null,
+    updated_at datetime not null,
     constraint pk_posts primary key (id),
     constraint fk_posts_users foreign key (user_id) references Users(id)
 )ENGINE=InnoDB;
@@ -30,8 +30,8 @@ create table if not exists Posts(
 create table if not exists Categories(
     id int(255) auto_increment not null,
     name varchar(255) not null,
-    created_at datetime,
-    updated_at datetime,
+    created_at datetime not null,
+    updated_at datetime not null,
     constraint pk_categories primary key (id)
 )ENGINE=InnoDB;
 
@@ -40,8 +40,8 @@ create table if not exists Comments(
     user_id int(255),
     post_id int(255),
     content longtext not null,
-    created_at datetime,
-    updated_at datetime,
+    created_at datetime not null,
+    updated_at datetime not null,
     constraint pk_comments primary key (id),
     constraint fk_comments_users foreign key (user_id) references Users(id),
     constraint fk_comments_posts foreign key (post_id) references Posts(id)
@@ -52,8 +52,8 @@ create table if not exists Reviews(
     user_id int(255),
     post_id int(255),
     comment_id int(255),
-    created_at datetime,
-    updated_at datetime,
+    created_at datetime not null,
+    updated_at datetime not null,
     constraint pk_reviews primary key (id),
     constraint fk_reviews_users foreign key (user_id) references Users(id),
     constraint fk_reviews_posts foreign key (post_id) references Posts(id),
