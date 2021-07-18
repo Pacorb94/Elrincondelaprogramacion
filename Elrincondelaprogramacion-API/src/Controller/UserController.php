a694c1db3d4c0141e05bc5c4a1f9d1ea5c0c93b0
Pacorb94 <a@a.com>
2021-07-18 22:51:51 +0200

    created controllers

---
+++ Elrincondelaprogramacion-API/src/Controller/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Validator\Constraints as Assert;
+use App\Entity\User;
+
+class UserController extends AbstractController
+{
+ 
+    
+}
