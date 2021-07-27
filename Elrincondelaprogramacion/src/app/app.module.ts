0b23d34aacc428771ab18a75048e7f742dce9eba
Pacorb94 <a@a.com>
2021-07-27 05:06:00 +0200

    modified app.module.ts

--- Elrincondelaprogramacion/src/app/app.module.ts
+++ Elrincondelaprogramacion/src/app/app.module.ts
@@ -1,16 +1,26 @@
+import { appRoutingProviders, routing } from './app.routing';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        NavbarComponent,
+        HomeComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        //Es un módulo
+        routing,
+        FlashMessagesModule.forRoot()
     ],
-    providers: [],
+    providers: [
+        appRoutingProviders
+    ],
     bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
