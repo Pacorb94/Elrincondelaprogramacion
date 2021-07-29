e18175450406bd04937e55271be0a8620c016cfb
Pacorb94 <a@a.com>
2021-07-29 22:24:33 +0200

    modified onAuthenticationSuccessResponse function

--- Elrincondelaprogramacion-API/src/Event/AuthenticationSuccessListener.php
+++ Elrincondelaprogramacion-API/src/Event/AuthenticationSuccessListener.php
@@ -6,6 +6,8 @@
     class AuthenticationSuccessListener{
 
         /**
+         * Función que se ejecuta cuando el usuario inicia sesión correctamente.
+         * En lugar de devolver el token (acción por defecto) devolvemos el usuario
          * @param AuthenticationSuccessEvent $event
          */
         public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
@@ -13,8 +15,8 @@
             $data = $event->getData();
             $user = $event->getUser();
             if (!$user instanceof UserInterface) return;   
-            $user->setCreatedAt($user->getCreatedAt()->format('d/m/Y H:i:s')); 
-            $user->setUpdatedAt($user->getUpdatedAt()->format('d/m/Y H:i:s')); 
+            $user->setCreatedAt($user->getCreatedAt()->format('Y-m-d H:i:s')); 
+            $user->setUpdatedAt($user->getUpdatedAt()->format('Y-m-d H:i:s')); 
             $event->setData([$user]);
         }
     }
