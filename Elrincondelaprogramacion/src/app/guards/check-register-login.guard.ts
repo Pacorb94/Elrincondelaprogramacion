28bfff6079cf96361359bcfe855ec8cefaad3298
Pacorb94 <a@a.com>
2021-08-02 16:49:08 +0200

    modified user-logged-in.guard.ts and created check-register-login.ts

---
+++ Elrincondelaprogramacion/src/app/guards/check-register-login.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { UserService } from './../services/user.service';
+import { Observable } from 'rxjs';
+
+
+@Injectable()
+export class CheckRegisterLoginGuard implements CanActivate {
+    private user:any;
+
+    constructor(private _userService:UserService, private _router:Router){
+        this.user=this._userService.getUserLoggedIn();
+    } 
+    
+    /**
+     * Función que restringe al usuario logueado ir al registro y login
+     * @returns 
+     */
+    canActivate() {
+        if (this.user) {
+            this._router.navigate(['/home']);
+            return false;
+        }
+        return true;
+    }
+}
