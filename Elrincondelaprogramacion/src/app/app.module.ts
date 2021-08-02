56c65fa6a74b006efb5385d106fa1a981dcee7e8
Pacorb94 <a@a.com>
2021-08-02 21:00:38 +0200

    modified app.module.ts

--- Elrincondelaprogramacion/src/app/app.module.ts
+++ Elrincondelaprogramacion/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { UserService } from './services/user.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -5,6 +6,9 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { FlashMessagesModule } from 'angular2-flash-messages';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MomentModule } from 'angular2-moment';
 
 @NgModule({
     declarations: [
@@ -15,9 +19,13 @@ import { FlashMessagesModule } from 'angular2-flash-messages';
     imports: [
         BrowserModule,
         AppRoutingModule,
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        MomentModule,
         FlashMessagesModule.forRoot()
     ],
-    providers: [],
+    providers: [UserService],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
