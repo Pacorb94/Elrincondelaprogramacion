305b53b35dc440858d3936a6a0c5d023013a376a
Pacorb94 <a@a.com>
2021-08-02 16:49:48 +0200

    created new functions

--- Elrincondelaprogramacion/src/app/services/User.service.ts
+++ Elrincondelaprogramacion/src/app/services/User.service.ts
@@ -1,9 +1,65 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable()
 export class UserService {
+    private url:string;
+    private user:BehaviorSubject<any>;
 
-    constructor() {
+    constructor(private _http:HttpClient) {
+        this.url='http://elrincondelaprogramacion.com/api';
+        this.user=new BehaviorSubject(this.getUserLoggedIn$());
+    }
 
+
+    setUserLoggedIn(value:any){
+        this.user.next(value);
+    }
+
+    /**
+     * Función que devuelve un observable para comprobar si el usuario inició
+     * sesión
+     * @returns 
+     */
+    getUserLoggedIn$():Observable<any>{
+        return this.user.asObservable(); 
+    }
+
+    /**
+     * Función que registra a un usuario
+     * @param user 
+     * @returns 
+     */
+    register(user:User):Observable<any>{
+        //Tenemos que convertir el usuario a json-string
+        let data=`json=${JSON.stringify(user)}`;
+        //Establecemos el tipo de cabecera
+        let headers=new HttpHeaders().set('Content-Type', 'application/json');
+        return this._http.post(`${this.url}/register`, data, {headers:headers});
+    }
+
+    /**
+     * Función que hace el inicio de sesión
+     * @param email 
+     * @param password 
+     * @returns 
+     */
+    login(email:string, password:string):Observable<any>{
+        let data={"email":email, "password":password};
+        let headers=new HttpHeaders().set('Content-Type', 'application/json');
+        return this._http.post(`${this.url}/login`, data, {headers:headers});
+    }
+
+    /**
+     * Función que obtiene el usuario logueado
+     * @return
+     */
+    getUserLoggedIn():any{
+        let user=null;
+        if (localStorage.hasOwnProperty('user')) {
+            user=JSON.parse(localStorage.getItem('user')||'{}');
+        }
+        return user;
     }
 }
