6cf5d49eacfa6b8fc1540af8a81aaf921cfcf8cf
Pacorb94 <a@a.com>
2021-08-02 23:17:25 +0200

    added user-panel module

--- Elrincondelaprogramacion/src/app/user/user.module.ts
+++ Elrincondelaprogramacion/src/app/user/user.module.ts
@@ -1,6 +1,6 @@
+import { UserPanelModule } from './user-panel/user-panel.module';
 import { CheckRegisterLoginGuard } from './../guards/check-register-login.guard';
 import { FlashMessagesModule } from 'angular2-flash-messages';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
@@ -8,7 +8,6 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
 
 @NgModule({
     declarations: [
@@ -20,7 +19,8 @@ import { HttpClientModule } from '@angular/common/http';
         ReactiveFormsModule,
         HttpClientModule,
         FlashMessagesModule.forRoot(),
-        UserRoutingModule
+        UserRoutingModule,
+        UserPanelModule
     ],
     providers:[CheckRegisterLoginGuard]
 })
