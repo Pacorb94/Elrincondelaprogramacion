3fde1db446e6c004dfdd564cbd469eb8b9631ce4
Pacorb94 <a@a.com>
2021-08-04 22:55:08 +0200

    modified app.module.ts

--- Elrincondelaprogramacion/src/app/app.module.ts
+++ Elrincondelaprogramacion/src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { UserService } from './services/user.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -8,6 +7,7 @@ import { HomeComponent } from './components/home/home.component';
 import { FlashMessagesModule } from 'angular2-flash-messages';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { UserService } from './modules/user/service/user.service';
 import { MomentModule } from 'angular2-moment';
 
 
