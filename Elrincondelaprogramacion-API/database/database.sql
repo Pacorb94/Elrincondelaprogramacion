637b1fb8f8f8ecd6a9b435a2ba48088f98917248
Pacorb94 <a@a.com>
2021-08-06 22:42:20 +0200

    modified script

--- Elrincondelaprogramacion-API/database/database.sql
+++ Elrincondelaprogramacion-API/database/database.sql
@@ -1,7 +1,7 @@
-create database if not exists `El_rincon_de_la_programacion`;
-use `El_rincon_de_la_programacion`;
+create database if not exists `el_rincon_de_la_programacion`;
+use `el_rincon_de_la_programacion`;
 
-create table if not exists Users(
+create table if not exists users(
     id int(255) auto_increment not null,
     nick varchar(50) unique not null,
     email varchar(50) unique not null,
@@ -14,7 +14,7 @@ create table if not exists Users(
     constraint pk_users primary key (id)
 )ENGINE=InnoDB;
 
-create table if not exists Posts(
+create table if not exists posts(
     id int(255) auto_increment not null,
     user_id int(255),
     category_id int (255),
@@ -25,20 +25,20 @@ create table if not exists Posts(
     created_at datetime not null,
     updated_at datetime not null,
     constraint pk_posts primary key (id),
-    constraint fk_posts_users foreign key (user_id) references Users(id)
+    constraint fk_posts_users foreign key (user_id) references users(id)
 )ENGINE=InnoDB;
 
-create table if not exists Categories(
+create table if not exists categories(
     id int(255) auto_increment not null,
     user_id int(255),
     name varchar(255) unique not null,
     created_at datetime not null,
     updated_at datetime not null,
     constraint pk_categories primary key (id),
-    constraint fk_categories_users foreign key (user_id) references Users(id)
+    constraint fk_categories_users foreign key (user_id) references users(id)
 )ENGINE=InnoDB;
 
-create table if not exists Comments(
+create table if not exists comments(
     id int(255) auto_increment not null,
     user_id int(255),
     post_id int(255),
@@ -47,8 +47,8 @@ create table if not exists Comments(
     created_at datetime not null,
     updated_at datetime not null,
     constraint pk_comments primary key (id),
-    constraint fk_comments_users foreign key (user_id) references Users(id),
-    constraint fk_comments_posts foreign key (post_id) references Posts(id)
+    constraint fk_comments_users foreign key (user_id) references users(id),
+    constraint fk_comments_posts foreign key (post_id) references posts(id)
 )ENGINE=InnoDB;
 
-alter table Posts add constraint fk_posts_categories foreign key (category_id) references Categories(id);
+alter table posts add constraint fk_posts_categories foreign key (category_id) references categories(id);
