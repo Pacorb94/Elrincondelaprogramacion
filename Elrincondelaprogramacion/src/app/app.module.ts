1d07676e306aef69301c43fcb5db64862c058e3e
Pacorb94 <a@a.com>
2021-08-07 20:59:36 +0200

    modified app.module.ts

--- Elrincondelaprogramacion/src/app/app.module.ts
+++ Elrincondelaprogramacion/src/app/app.module.ts
@@ -24,7 +24,7 @@ import { MomentModule } from 'angular2-moment';
         ReactiveFormsModule,
         HttpClientModule,
         MomentModule,
-        FlashMessagesModule.forRoot()
+        FlashMessagesModule.forRoot(),
     ],
     providers: [
         UserService, 
