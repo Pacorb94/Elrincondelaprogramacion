ea9c9c9e26788191c5440dbf7dd69a128aaaab65
Pacorb94 <a@a.com>
2021-08-09 13:27:03 +0200

    added getPosts and getCategories functions

--- Elrincondelaprogramacion-API/src/Controller/HomeController.php
+++ Elrincondelaprogramacion-API/src/Controller/HomeController.php
@@ -3,11 +3,30 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Post;
+use App\Entity\Category;
 
 class HomeController extends AbstractController
 {
-    
-    
+    /**
+     * Función que obtiene todos los posts
+     * @return JsonResponse
+     */
+    public function getPosts()
+    {
+        $postRepo=$this->getDoctrine()->getRepository(Post::class);
+        $posts=$postRepo->findAll();
+        return $this->json($posts);
+    }
+
+    /**
+     * Función que obtiene las categorías
+     * @return JsonResponse
+     */
+    public function getCategories()
+    {
+        $categoryRepo=$this->getDoctrine()->getRepository(Category::class);
+        $categories=$categoryRepo->findAll();
+        return $this->json($categories);
+    }
 }
