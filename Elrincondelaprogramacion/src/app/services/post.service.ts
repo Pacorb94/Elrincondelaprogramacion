bc2285946958f0c4850aad9fe590fd14857c26c3
Pacorb94 <a@a.com>
2021-08-09 14:50:43 +0200

    created post.service.ts

---
+++ Elrincondelaprogramacion/src/app/services/post.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from './../../environments/environment';
+
+@Injectable()
+export class PostService {
+
+    constructor(private _http:HttpClient) {
+        
+    }
+
+    /**
+     * Función que obtiene los posts
+     * @returns 
+     */
+    getPosts():Observable<any>{
+        let header=new HttpHeaders().set('Content-Type', 'application/json');
+        return this._http.get(`${environment.url}/posts`, {headers:header});
+    }
+}
