90e8f1d4c9a0859790582dd6f07d8fe975890fbf
Pacorb94 <a@a.com>
2021-08-09 22:52:00 +0200

    modified panel-routing.module.ts

--- Elrincondelaprogramacion/src/app/modules/user/module/panel/panel-routing.module.ts
+++ Elrincondelaprogramacion/src/app/modules/user/module/panel/panel-routing.module.ts
@@ -2,6 +2,8 @@ import { MainComponent } from './components/main/main.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserSettingsComponent } from './components/user-settings/user-settings.component';
+import { MyPostsComponent } from './components/my-posts/my-posts.component';
+
 
 const routes: Routes = [
     {
@@ -9,6 +11,9 @@ const routes: Routes = [
         children:[
             {
                 path:'user-settings', component:UserSettingsComponent
+            },
+            {
+                path:'my-posts', component:MyPostsComponent
             },
             {
                 path:'**', redirectTo:''
