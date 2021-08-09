9d7c8d159a22f1164a11e826ef671ec1a36ec659
Pacorb94 <a@a.com>
2021-08-09 13:54:09 +0200

    modified app-routing.module.ts

--- Elrincondelaprogramacion/src/app/app-routing.module.ts
+++ Elrincondelaprogramacion/src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import { HomeComponent } from './components/home/home.component';
 
 //Definimos las rutas
 const routes: Routes = [
-    { path: '', component: HomeComponent },
+    { path:'', component:HomeComponent },
     //Cargamos una ruta padre que tendrá rutas hijas con lo cual usará el lazyLoading
-    { path: '', loadChildren: ()=>import('./modules/user/user.module').then(m=>m.UserModule) },
+    { path:'', loadChildren:()=>import('./modules/user/user.module').then(m=>m.UserModule) },
+    { path:'', loadChildren:()=>import('./modules/post/post.module').then(m=>m.PostModule) },
     { path:'**', redirectTo:''}
 ];
 
