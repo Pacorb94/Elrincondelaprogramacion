fd6b9ae13015217766826c72bb5517254fd75b17
Pacorb94 <a@a.com>
2021-08-09 20:00:24 +0200

    created my-footer component

---
+++ Elrincondelaprogramacion/src/app/components/my-footer/my-footer.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'my-footer',
+    templateUrl: './my-footer.component.html',
+    styleUrls: ['./my-footer.component.scss']
+})
+export class MyFooterComponent implements OnInit {
+
+    constructor() {
+
+    }
+
+    ngOnInit(): void {
+    }
+
+}
