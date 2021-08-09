ec262f44988c9e7c9f852f37959766450bb7072e
Pacorb94 <a@a.com>
2021-08-09 20:00:54 +0200

    created last-added-categories component

---
+++ Elrincondelaprogramacion/src/app/components/last-added-categories/last-added-categories.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-last-added-categories',
+  templateUrl: './last-added-categories.component.html',
+  styleUrls: ['./last-added-categories.component.scss']
+})
+export class LastAddedCategoriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
