209c0e67d5f328cd9b031c385fe1834974edee06
Pacorb94 <a@a.com>
2021-08-09 14:46:39 +0200

    deleted PostModule routes

--- Elrincondelaprogramacion/src/app/app-routing.module.ts
+++ Elrincondelaprogramacion/src/app/app-routing.module.ts
@@ -7,7 +7,6 @@ const routes: Routes = [
     { path:'', component:HomeComponent },
     //Cargamos una ruta padre que tendrá rutas hijas con lo cual usará el lazyLoading
     { path:'', loadChildren:()=>import('./modules/user/user.module').then(m=>m.UserModule) },
-    { path:'', loadChildren:()=>import('./modules/post/post.module').then(m=>m.PostModule) },
     { path:'**', redirectTo:''}
 ];
 
