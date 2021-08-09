7916a5c54c6c8a16fef79775c678f371f411feeb
Pacorb94 <a@a.com>
2021-08-09 22:51:22 +0200

    modified my-posts.component.ts

--- Elrincondelaprogramacion/src/app/modules/user/module/panel/components/my-posts/my-posts.component.ts
+++ Elrincondelaprogramacion/src/app/modules/user/module/panel/components/my-posts/my-posts.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/modules/user/service/user.service';
+import { PostService } from './../../../../../../services/post.service';
+
 
 @Component({
-  selector: 'app-my-posts',
-  templateUrl: './my-posts.component.html',
-  styleUrls: ['./my-posts.component.scss']
+    selector: 'my-posts',
+    templateUrl: './my-posts.component.html',
+    styleUrls: ['./my-posts.component.scss']
 })
-export class MyPostsComponent implements OnInit {
+export class MyPostsComponent implements OnInit{
+    user:any;
+    posts:any[];
 
-  constructor() { }
+    constructor(private _userService:UserService, private _postService:PostService) {
+        this.loadUser();
+        this.posts=[];
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(){
+        this.getPostsByUser();
+    }
+    
+    /**
+     * Función que carga el usuario
+     */
+    loadUser(){
+        this._userService.getUserLoggedIn$().subscribe(
+            user=>{
+                if (user) this.user=user;
+            }
+        );
+    }
 
+    /**
+     * Función que obtiene los posts del usuario
+     */
+    getPostsByUser(){
+        this._postService.getPostsByUser(this.user.id).subscribe(
+            response=>{
+                if (response) {
+                   
+                  
+                    console.log(response);
+                }
+            },
+            error=>{
+
+            }
+        );
+    }
 }
