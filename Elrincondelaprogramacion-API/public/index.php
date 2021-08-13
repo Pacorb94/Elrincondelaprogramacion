0646143b02b655418a40ee1730e9f2fae2812c66
Pacorb94 <a@a.com>
2021-08-13 04:13:44 +0200

    deleted headers

--- Elrincondelaprogramacion-API/public/index.php
+++ Elrincondelaprogramacion-API/public/index.php
@@ -1,14 +1,11 @@
 <?php
 
 use App\Kernel;
-header("Access-Control-Allow-Origin: https://localhost:4200");
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization, authorization");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+
 if ($_SERVER['REQUEST_METHOD']=='OPTIONS') die();
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
+?>
