a8b0cb150294d389f65a0404a4fd5e35dcbd9d3a
Pacorb94 <a@a.com>
2021-08-13 19:57:51 +0200

    modified create-category.component.ts

--- Elrincondelaprogramacion/src/app/modules/user/module/panel/components/create-category/create-category.component.ts
+++ Elrincondelaprogramacion/src/app/modules/user/module/panel/components/create-category/create-category.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-create-category',
+  selector: 'create-category',
   templateUrl: './create-category.component.html',
   styleUrls: ['./create-category.component.scss']
 })
