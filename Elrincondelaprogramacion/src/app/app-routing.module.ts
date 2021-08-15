0484d09d32ab4fbc0ae2605bb00a8c8d4ea26055
Pacorb94 <a@a.com>
2021-08-16 00:24:08 +0200

    modified app-routing.module.ts

--- Elrincondelaprogramacion/src/app/app-routing.module.ts
+++ Elrincondelaprogramacion/src/app/app-routing.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { PostsListComponent } from './components/posts-list/posts-list.component';
 
-//Definimos las rutas
 const routes: Routes = [
     { path:'', component:HomeComponent },
+    { path:'page/:page', component:PostsListComponent },
     //Cargamos una ruta padre que tendrá rutas hijas con lo cual usará el lazyLoading
     { path:'', loadChildren:()=>import('./modules/user/user.module').then(m=>m.UserModule) },
     { path:'**', redirectTo:''}
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    /*Cada vez que se pulse en un ancla nos llevará suavemente 
+    hacia la parte superior de la pantalla*/
+    imports: [RouterModule.forRoot(routes, {
+        scrollPositionRestoration: 'top'
+    })],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
