b7223bcd865f493b48be61a946e913dc48a1f9da
Pacorb94 <a@a.com>
2021-08-15 06:03:38 +0200

    added getCategories function

--- Elrincondelaprogramacion/src/app/modules/category/service/category.service.ts
+++ Elrincondelaprogramacion/src/app/modules/category/service/category.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn:'root'
@@ -15,6 +15,7 @@ export class CategoryService {
      * @returns 
      */
     getCategories():Observable<any>{
-        return this._http.get(`${environment.url}/categories`);
+        let header=new HttpHeaders().set('Content-Type', 'application/json');
+        return this._http.get(`${environment.url}/categories`, {headers:header});
     }
 }
