c4984d4e4383d1d474a575523a6d223541cc157c
Pacorb94 <a@a.com>
2021-08-15 20:13:10 +0200

    created create and delete functions

--- Elrincondelaprogramacion/src/app/modules/category/service/category.service.ts
+++ Elrincondelaprogramacion/src/app/modules/category/service/category.service.ts
@@ -18,4 +18,24 @@ export class CategoryService {
         let header=new HttpHeaders().set('Content-Type', 'application/json');
         return this._http.get(`${environment.url}/categories`, {headers:header});
     }
+
+    /**
+     * Función que crea un categoría
+     * @param category 
+     * @returns 
+     */
+    create(category:any):Observable<any>{
+        let data=`json=${JSON.stringify(category)}`;
+        let header=new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+        return this._http.post(`${environment.url}/category/create`, data, {headers:header});
+    }
+
+    /**
+     * Función que borra una categoría
+     * @param id 
+     * @returns 
+     */
+    delete(id:number):Observable<any>{
+        return this._http.delete(`${environment.url}/categories/${id}/delete`);
+    }
 }
