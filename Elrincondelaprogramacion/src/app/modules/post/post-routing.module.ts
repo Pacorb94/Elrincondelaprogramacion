e63e87395022455c6ad7aa3db00e715364dfc120
Pacorb94 <a@a.com>
2021-08-17 01:53:04 +0200

    added new routes

--- Elrincondelaprogramacion/src/app/modules/post/post-routing.module.ts
+++ Elrincondelaprogramacion/src/app/modules/post/post-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostsListComponent } from './components/posts-list/posts-list.component';
+import { PostsByCategoryComponent } from './components/posts-by-category/posts-by-category.component';
 
 const routes: Routes = [
-    { path: 'page/:page', component: PostsListComponent }
+    { path: 'page/:page', component: PostsListComponent },
+    { path: 'posts/categories/:name', component: PostsByCategoryComponent},
+    { path: '**', redirectTo:''}
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
+    imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
 export class PostRoutingModule { }
