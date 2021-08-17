a02d64cd26e9a1af611a5115d1e6d318fafba368
Pacorb94 <a@a.com>
2021-08-17 21:13:29 +0200

    created cookies-policy component

---
+++ Elrincondelaprogramacion/src/app/components/cookies-policy/cookies-policy.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'cookies-policy',
+    templateUrl: './cookies-policy.component.html',
+    styleUrls: ['./cookies-policy.component.scss']
+})
+export class CookiesPolicyComponent implements OnInit {
+
+    constructor() {}
+
+    ngOnInit(): void {
+    }
+}
