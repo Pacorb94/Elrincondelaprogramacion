58d6e25e9ddf1e498a6f36d009c5eb3d9178cd86
Pacorb94 <a@a.com>
2021-08-18 00:29:13 +0200

    getLastCreatedCategory and checkDates functions

--- Elrincondelaprogramacion/src/app/components/last-added-category/last-added-category.component.ts
+++ Elrincondelaprogramacion/src/app/components/last-added-category/last-added-category.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { CategoryService } from './../../services/category.service';
 
 @Component({
     selector: 'last-added-category',
     templateUrl: './last-added-category.component.html',
     styleUrls: ['./last-added-category.component.scss']
 })
-export class LastAddedCategoryComponent implements OnInit {
+export class LastAddedCategoryComponent implements OnInit, OnDestroy {
+    category:any;
+    subscription:Subscription;
 
-    constructor() { }
-
-    ngOnInit(): void {
+    constructor(private _categoryService:CategoryService) { 
+        this.subscription=new Subscription();
     }
 
+    ngOnInit(): void {
+        this.getLastCreatedCategory();
+      //  this.checkDates();
+    }
+
+    ngOnDestroy(){
+        this.subscription.unsubscribe();
+    }
+    
+    getLastCreatedCategory(){
+        this.subscription=this._categoryService.getLastAddedCategory$().subscribe(
+            category=>{
+                if (category) {
+                    this.category=category;
+                    let categoryDate=new Date(this.category.createdAt);
+                    let categoryDatePlusOneWeek=new Date(categoryDate.setDate(categoryDate.getDate()+7));
+                    localStorage.setItem('oneWeek', categoryDatePlusOneWeek.toString());                 
+                }
+                this.checkDates();
+            }
+        );
+    }
+
+    checkDates(){
+        if (localStorage.hasOwnProperty('oneWeek')) {
+            let oneWeek=new Date(localStorage.getItem('oneWeek')??'');
+            if (+oneWeek===+new Date()) {
+                console.log('iguales');
+            }else{
+                console.log('no iguales');
+            }
+
+        }
+    }
 }
