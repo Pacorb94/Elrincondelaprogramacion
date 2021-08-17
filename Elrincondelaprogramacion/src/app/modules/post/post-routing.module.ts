b1774d3cdb5a2423823751f8d3b9cba263759707
Pacorb94 <a@a.com>
2021-08-17 06:57:27 +0200

    modified post-routing.module.ts

--- Elrincondelaprogramacion/src/app/modules/post/post-routing.module.ts
+++ Elrincondelaprogramacion/src/app/modules/post/post-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostsListComponent } from './components/posts-list/posts-list.component';
-import { PostsByCategoryComponent } from './components/posts-by-category/posts-by-category.component';
 
 const routes: Routes = [
     { path: 'page/:page', component: PostsListComponent },
-    { path: 'posts/categories/:name', component: PostsByCategoryComponent}  
+    { path: 'posts/categories/:name', component: PostsListComponent }  
 ];
 
 @NgModule({
