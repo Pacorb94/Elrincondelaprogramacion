173fa41eac5b1b2f9c8433adc5a3535a8ac501c0
Pacorb94 <a@a.com>
2021-08-17 07:00:09 +0200

    modified post.module.ts

--- Elrincondelaprogramacion/src/app/modules/post/post.module.ts
+++ Elrincondelaprogramacion/src/app/modules/post/post.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostsListComponent } from './components/posts-list/posts-list.component';
 import { PostRoutingModule } from './post-routing.module';
-import { PostsByCategoryComponent } from './components/posts-by-category/posts-by-category.component';
 
 @NgModule({
     declarations: [
-        PostsListComponent,
-        PostsByCategoryComponent
+        PostsListComponent
     ],
     imports: [
         CommonModule,
