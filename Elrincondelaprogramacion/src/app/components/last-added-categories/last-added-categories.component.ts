74bc1065049cb45ebcb3ad8a0863c632e6df640f
Pacorb94 <a@a.com>
2021-08-18 20:22:57 +0200

    moved from category module

---
+++ Elrincondelaprogramacion/src/app/components/last-added-categories/last-added-categories.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'last-added-categories',
+  templateUrl: './last-added-categories.component.html',
+  styleUrls: ['./last-added-categories.component.scss']
+})
+export class LastAddedCategoriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
