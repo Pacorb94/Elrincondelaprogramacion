a739d2b66256194a3ea63f0077250ceefc8f6253
Pacorb94 <a@a.com>
2021-08-18 20:20:16 +0200

    added new route

--- Elrincondelaprogramacion/src/app/app-routing.module.ts
+++ Elrincondelaprogramacion/src/app/app-routing.module.ts
@@ -1,30 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { PostRoutingModule } from './modules/post/post-routing.module';
 import { CookiesPolicyComponent } from './components/cookies-policy/cookies-policy.component';
 
 
 const routes: Routes = [
     { path:'', component:HomeComponent },
-    { path: 'page/:page', component: HomeComponent },
-    { path: 'posts/categories/:name', component: HomeComponent },
+    { path:'page/:page', component:HomeComponent },
+    { path:'posts/categories/:name', component:HomeComponent },
+    { path:'posts/categories/:name/page/:page', component:HomeComponent },
     { path:'cookies-policy', component:CookiesPolicyComponent },
     //Cargamos una ruta padre que tendrá rutas hijas con lo cual usará el lazyLoading
-    { path:'', loadChildren:()=>import('./modules/user/user.module').then(m=>m.UserModule) },
+    { path:'', loadChildren:()=>import('./module/user/user.module').then(m=>m.UserModule) },
     { path:'**', redirectTo:''}
 ];
 
 
 @NgModule({
+    //Para que funcionen las rutas de otros módulos debemos importarlas
     imports: [
         /*Cada vez que se pulse en un ancla nos llevará suavemente 
         hacia la parte superior de la pantalla*/
         RouterModule.forRoot(routes, {
             scrollPositionRestoration: 'top'
-        }),
-        //Para que funcionen las rutas de este módulo debemos importarlas
-        PostRoutingModule
+        })
     ],
     exports: [RouterModule]
 })
