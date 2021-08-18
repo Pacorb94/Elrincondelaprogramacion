f46ff4e9c496fe37f486cd88ce7a246777e6385d
Pacorb94 <a@a.com>
2021-08-18 20:23:30 +0200

    moved from post module

---
+++ Elrincondelaprogramacion/src/app/components/most-active-posts/most-active-posts.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'most-active-posts',
+	templateUrl: './most-active-posts.component.html',
+	styleUrls: ['./most-active-posts.component.scss']
+})
+export class MostActivePostsComponent implements OnInit {
+
+	constructor() {
+
+	}
+
+	ngOnInit(): void {
+	}
+
+}
