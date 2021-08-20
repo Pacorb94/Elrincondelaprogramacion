ac071f8ae7878101d6b3620e9da0846024e54f70
Pacorb94 <a@a.com>
2021-08-20 16:44:00 +0200

    created watch-post component

---
+++ Elrincondelaprogramacion/src/app/components/watch-post/watch-post.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'watch-post',
+    templateUrl: './watch-post.component.html',
+    styleUrls: ['./watch-post.component.scss']
+})
+export class WatchPostComponent implements OnInit {
+
+    constructor() {
+
+    }
+
+    ngOnInit(): void {
+    }
+
+}
