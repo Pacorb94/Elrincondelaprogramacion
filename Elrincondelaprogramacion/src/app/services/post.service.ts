2a43b682c19833aab1f2e1ed8e8ed34fb8c10879
Pacorb94 <a@a.com>
2021-08-21 05:32:00 +0200

    added inadequate function

--- Elrincondelaprogramacion/src/app/services/post.service.ts
+++ Elrincondelaprogramacion/src/app/services/post.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
+import { Post } from '../models/Post';
+import { environment } from '../../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
-import { Post } from '../models/Post';
 
 
 @Injectable({
@@ -69,6 +69,18 @@ export class PostService {
         let data=`json=${JSON.stringify(post)}`;
         let header=new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
         return this._http.put(`${environment.url}/posts/${post.id}/update`, data, {headers:header});
+    }
+
+    /**
+     * Función que marca como inadecuado un post
+     * @param id 
+     * @param inadequate 
+     * @returns 
+     */
+    inadequate(id:number, inadequate:string):Observable<any>{
+        let data=`json=${inadequate}`;
+        let header=new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+        return this._http.put(`${environment.url}/posts/${id}/inadequate`, data, {headers:header});
     }
 
     /**
