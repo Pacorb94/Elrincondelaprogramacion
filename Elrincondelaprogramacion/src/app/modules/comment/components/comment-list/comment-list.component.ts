985f6461adc80be28884bfbe1125fa3e0ea4f696
Pacorb94 <a@a.com>
2021-08-22 00:10:42 +0200

    modified comment-list.component.ts

--- Elrincondelaprogramacion/src/app/modules/comment/components/comment-list/comment-list.component.ts
+++ Elrincondelaprogramacion/src/app/modules/comment/components/comment-list/comment-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit,  } from '@angular/core';
+import { CommentService } from '../../service/comment.service';
 
 @Component({
     selector: 'comment-list',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./comment-list.component.scss']
 })
 export class CommentListComponent implements OnInit {
+    @Input()posts:any[];
 
-    constructor() { }
+    constructor(private _commentService:CommentService) { 
 
-    ngOnInit(): void {
     }
 
+    ngOnInit(): void {
+
+    }
+
+    updateComment(id:number){
+
+    }
+
+    deleteComment(id:number){
+
+    }
 }
