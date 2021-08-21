b6c731e9995a6b720125c9ce0d40f26efec40367
Pacorb94 <a@a.com>
2021-08-21 17:08:37 +0200

    created user module

---
+++ Elrincondelaprogramacion/src/app/modules/user/module/panel/components/banned-users/banned-users.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'banned-users',
+    templateUrl: './banned-users.component.html',
+    styleUrls: ['./banned-users.component.scss']
+})
+export class BannedUsersComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+}
---
+++ Elrincondelaprogramacion/src/app/modules/user/module/panel/panel-routing.module.ts
@@ -0,0 +1,53 @@
+import { EditCategoryComponent } from './components/edit-category/edit-category.component';
+import { CreateCategoryComponent } from './components/create-category/create-category.component';
+import { EditPostComponent } from './components/edit-post/edit-post.component';
+import { MainComponent } from './components/main/main.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UserSettingsComponent } from './components/user-settings/user-settings.component';
+import { MyPostsComponent } from './components/my-posts/my-posts.component';
+import { CreatePostComponent } from './components/create-post/create-post.component';
+import { CategoryListComponent } from './components/category-list/category-list.component';
+import { CheckAdminRoleGuard } from './../../guards/check-admin-role.guard';
+import { CheckNoReaderRoleGuard } from './../../guards/check-no-reader-role.guard';
+
+const routes: Routes = [
+    {
+        path: '', component:MainComponent,
+        children:[
+            {
+                path:'my-posts', component:MyPostsComponent, canActivate:[CheckNoReaderRoleGuard]
+            },
+            {
+                path:'category-list', component:CategoryListComponent, canActivate:[CheckAdminRoleGuard]
+            },
+            {
+                path:'create-post', component:CreatePostComponent, canActivate:[CheckNoReaderRoleGuard]
+            },
+            {
+                path:'create-category', component:CreateCategoryComponent, 
+                canActivate:[CheckAdminRoleGuard]
+            },
+            {
+                path:'edit-post/:title', component:EditPostComponent, 
+                canActivate:[CheckNoReaderRoleGuard]
+            },
+            {
+                path:'edit-category/:name', component:EditCategoryComponent, 
+                canActivate:[CheckAdminRoleGuard]
+            },
+            {
+                path:'user-settings', component:UserSettingsComponent
+            },
+            {
+                path:'**', redirectTo:''
+            }
+        ]
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class UserPanelRoutingModule { }
