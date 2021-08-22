c78c97d8c10ffeeafc38c84964d08812839593dc
Pacorb94 <a@a.com>
2021-08-22 17:01:31 +0200

    modified imports

--- Elrincondelaprogramacion/src/app/modules/comment/comment.module.ts
+++ Elrincondelaprogramacion/src/app/modules/comment/comment.module.ts
@@ -3,7 +3,10 @@ import { CommonModule } from '@angular/common';
 import { CommentListComponent } from './components/comment-list/comment-list.component';
 import { CreateCommentComponent } from './components/create-comment/create-comment.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { FlashMessagesModule } from 'angular2-flash-messages';
+import { MomentModule } from 'angular2-moment';
 
 @NgModule({
     declarations: [
@@ -13,7 +16,10 @@ import { FlashMessagesModule } from 'angular2-flash-messages';
     imports: [
         CommonModule,
         HttpClientModule,
-        FlashMessagesModule.forRoot()
+        ReactiveFormsModule,
+        RouterModule,
+        FlashMessagesModule.forRoot(),
+        MomentModule
     ],
     exports:[
         CommentListComponent,
