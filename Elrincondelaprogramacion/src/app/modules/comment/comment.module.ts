c799effb3cccaffa2a90b4ac68a18f0badd602f5
Pacorb94 <a@a.com>
2021-08-22 05:39:17 +0200

    remove component

--- Elrincondelaprogramacion/src/app/modules/comment/comment.module.ts
+++ Elrincondelaprogramacion/src/app/modules/comment/comment.module.ts
@@ -1,22 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CommentListComponent } from './components/comment-list/comment-list.component';
-import { CreateCommentComponent } from './components/create-comment/create-comment.component';
+import { CreateCommentComponent } from './component/create-comment/create-comment.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MomentModule } from 'angular2-moment';
 import { FlashMessagesModule } from 'angular2-flash-messages';
 
 @NgModule({
     declarations: [
-        CommentListComponent,
         CreateCommentComponent
     ],
     imports: [
         CommonModule,
         HttpClientModule,
+        MomentModule,
         FlashMessagesModule.forRoot()
     ],
     exports:[
-        CommentListComponent,
         CreateCommentComponent
     ]
 })
