a796c80faa9f7d538a71caa460cd3ab8019de65e
Pacorb94 <a@a.com>
2021-08-22 20:50:05 +0200

    modified comment-list.component.ts

--- Elrincondelaprogramacion/src/app/modules/comment/components/comment-list/comment-list.component.ts
+++ Elrincondelaprogramacion/src/app/modules/comment/components/comment-list/comment-list.component.ts
@@ -1,19 +1,28 @@
-import { Component, Input, OnInit,  } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { UserService } from './../../../user/service/user.service';
 import { CommentService } from '../../service/comment.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'comment-list',
     templateUrl: './comment-list.component.html',
     styleUrls: ['./comment-list.component.scss']
 })
-export class CommentListComponent implements OnInit {
-    @Input()posts:any[];
+export class CommentListComponent implements OnInit, OnDestroy {
+    @Input()post:any;
+    user:any;
+    deleteCommentSubscription:Subscription;
 
-    constructor(private _commentService:CommentService) { 
-
+    constructor(private _userService:UserService, private _commentService:CommentService) { 
+        this.user=this._userService.getUserLoggedIn();
+        this.deleteCommentSubscription=new Subscription();
     }
 
     ngOnInit(): void {
+
+    }
+
+    ngOnDestroy(){
 
     }
 
