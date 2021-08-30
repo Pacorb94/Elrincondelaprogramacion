265796c905cee23b6c68e5bb634fb0d5fe9dcc11
Pacorb94 <a@a.com>
2021-08-30 20:35:26 +0200

    banned-users component renamed to users-to-ban

---
+++ Elrincondelaprogramacion/src/app/modules/user/module/panel/components/users-to-ban/users-to-ban.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'users-to-ban',
+    templateUrl: './users-to-ban.component.html',
+    styleUrls: ['./users-to-ban.component.scss']
+})
+export class UsersToBanComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+}
